3708c145c25c45c215de5d0ac666453f83e56c04
Martin Wiedermann <martin.wiedermann1@gmail.com>
2022-03-14 08:25:21 +0100

    idk dude i was mentally dead yesterday and only wrote garbage code mostly

--- resources/scripts/script1.js
+++ resources/scripts/script1.js
@@ -37,48 +37,57 @@ const createCard = (id) => {
     
 }
 
-const header = document.querySelector('header');
 
 //TODO has to be fixed when stuff besdies sidebar generates card on click
 
 
 const sidebarClickables = document.getElementsByClassName('--sidebar-clickable');
+const openInNewTab = document.getElementsByClassName('--card-h-newtab');
+const closeCard = document.getElementsByClassName('--card-h-x');
+const sidebar = document.getElementById('sidebar');
+const main = document.querySelector('main');
+const header = document.querySelector('header');
+const sidebarText = document.getElementsByClassName('--sidebar-clickable-text');
+const arrow = document.getElementById('expand');
+//
+const clickables = [sidebarClickables, openInNewTab, closeCard];
 
 
-
-const cardGeneratingClickables = [sidebarClickables];
-
-for(let j = 0 ; j < cardGeneratingClickables.length ; j += 1){
-
-
-
-
-
-    for(let i = 0 ; i < cardGeneratingClickables[0].length ; i += 1){
-
-        let cE = cardGeneratingClickables[0][i];
-        let id = cE.id;
-
-        cE.onclick = ((e) => {
-
-            cE.style.cursor = 'pointer';
-
-            if(cE.id != 'expand'){
-                createCard(id);
-            }
-        })
-        cE.onmouseover = ((e) => {
-            cE.style.border = 'var(--large-border)';
-
-        })
-        cE.onmouseleave = ((e) => {
-            cE.style.border = 'none';
-        })
-
-
+arrow.onclick = ((e) => {
+    /* target grid area => sidebar: 1 / 1 / -1 / 3; ; header: 1 / 3 / 2 / -1; main: 2 / 3 / -1 / -1;
+    */
+    sidebar.style.gridArea = '1 / 1 / -1 / 3';
+    header.style.gridArea = '1 / 3 / 2 / -1';
+    main.style.gridArea = '2 / 3 / -1 / -1';
+    arrow.id = 'collapse';
+    arrow.title = 'collapse';
+    
+    for(let k = 0 ; k < sidebarText.length ; k += 1){
+        console.log('ay');
+        sidebarText[k].style.display = 'flex';
     }
-}
+    
+    arrow.style.cursor = 'pointer';
+
+    sidebarClickables.forEach((e) => {
+        e.style.width = '2.5cm';
+    })
+})
 
 
+
+    
+    cE.onclick = ((e) => {
+        cE.style.cursor = 'pointer';
+        if(cE.id != 'expand'){
+            createCard(id);
+        }
+    })
+    cE.onmouseover = ((e) => {
+        cE.style.border = 'var(--large-border)';
+    })
+    cE.onmouseleave = ((e) => {
+        cE.style.border = 'none';
+    })
 
 
